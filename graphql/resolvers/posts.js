5a4d7e136c2e29ace308b8e283dd956dbb7408b6
Sahil <sahilkumarpcm@gmail.com>
2020-10-05 22:46:14 +0530

    itin graphql setup

---
+++ graphql/resolvers/posts.js
@@ -0,0 +1,14 @@
+const Post = require("../../models/Posts");
+
+module.exports = {
+  Query: {
+    getPosts: async () => {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+};
