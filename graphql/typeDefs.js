41d11101c6c9d25000006e7fda0da7316ec150be
Sahil <sahilkumarpcm@gmail.com>
2020-10-07 22:59:24 +0530

    changes

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -47,7 +47,7 @@ module.exports = gql`
     deleteComment(postId: ID!, commentId: ID!): Post!
     likePost(postId: ID!): Post!
   }
-  type Subscription {
-    newPost: Post!
-  }
+  # type Subscription {
+  #   newPost: Post!
+  # }
 `;
