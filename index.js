17f949f0b93fcbaa5b7ae50fd3e2777b4b0b3feb
Sahil <sahilkumarpcm@gmail.com>
2020-10-09 17:27:16 +0530

    add client nav and configure apollo client

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("mongodb connected");
-    return server.listen({ port: 5000 });
+    return server.listen({ port: 3000 });
   })
   .then((res) => {
     console.log(`server running at ${res.url}`);
-  });
+  })
+  .catch((err) => console.log(err));
